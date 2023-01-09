00b94fdd41cefbd5b454111f7424c312ce095334
Pan Li <pan.li@nih.gov>
2023-01-09 13:56:00 -0500

    Update readLOOPmv.m

--- readLOOPmv.m
+++ readLOOPmv.m
@@ -1,6 +1,6 @@
 % A 1D Tissue Model of the Cardiac Purkinje-Ventricular System
 % by Mengya Yuan, Heqiang Lian, and Pan Li*, 
-% as in Spatiotemporal Patterns of Early Afterdepolarizations Underlying Abnormal T-wave Morphologies in a Tissue Model of the Purkinje-Ventricular System, PLoS ONE, Jan 2023
+% as in Yuan M, Lian H, Li P (2023) Spatiotemporal patterns of early afterdepolarizations underlying abnormal T-wave morphologies in a tissue model of the Purkinje-ventricular system. PLoS ONE 18(1): e0280267. https://doi.org/10.1371/journal.pone.0280267
 % *Email: pan.li@nih.gov
 
 % To plot use matlab:
